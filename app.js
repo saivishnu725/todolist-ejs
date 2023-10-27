eb20dcce5d8635c877a41c5918951ca33be583d6
Sai Vishnu <saivishnu725@gmail.com>
2023-10-27 11:00:42 +0530

    remove switch to be smort

--- app.js
+++ app.js
@@ -19,33 +19,8 @@ app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
   let today = new Date();
-  let currentDay = today.getDay();
   let day = "";
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    default:
-      console.log("Error: current day is equal to: " + currentDay);
-  }
+  day = today.toLocaleDateString("en-us", { weekday: "long" });
   res.render("list", { day: day });
 });
 
